19ef8e2535dc614ff05f47ae36a4b982a8b5eaf6
Gowri Shankar <shankarg.kvsulur@gmail.com>
2019-11-26 23:42:39 +0530

    Adding middlewares

--- src/index.js
+++ src/index.js
@@ -13,14 +13,12 @@ const rootReducer = combineReducers({
   res: resultReducer
 });
 
-
-
 const logger = store => {
   return next => {
     return action => {
       console.log("[Middleware] Dispatching", action);
       const result = next(action);
-      console.log("[Middleware] next state", store.getstate());
+      console.log("[Middleware] next state", store.getState());
       return result;
     };
   };
